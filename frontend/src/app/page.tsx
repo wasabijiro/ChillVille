6167b42a790450762eb8ab294d63ca32f470389e
wasabi <0xwasabijiro@gmail.com>
2024-09-22 02:31:58 +0800

    nouns design frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -18,6 +18,7 @@ import { delay } from "@/utils/delay";
 import { ETHEREUM_SEPOLIA_ZKPASS_ADDRESS } from "@/config/contract";
 import { ZKPASS_APP_ID, ZKPASS_SCHEMA_ID } from "@/config/zkpass";
 import zkPassABI from "@/config/zkPassABI.json";
+import { NounsGlasses } from "@/components/NounsGlasses";
 
 export default function Home() {
   const votingUrl = process.env.NEXT_PUBLIC_VOTING_URL || '#';
@@ -172,22 +173,23 @@ export default function Home() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-900 text-white">
+    <div className="min-h-screen bg-yellow-300 text-gray-800">
       <main className="py-8">
         <div className="px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-10 flex items-center">
             <div className="flex-1 text-center">
+              <NounsGlasses />
               <h1 className="text-4xl font-bold mb-2">
                 Private Onchain Voting with Sybil Resistance
                 <br />
                 MACI meets zkTLS
               </h1>
-              <p className="text-xl text-gray-400 mb-1">
+              <p className="text-xl text-gray-600 mb-1">
                 Verify your offchain activity & Vote without exposure
               </p>
             </div>
             <button
-              className="bg-blue-400 py-1 px-3 rounded-lg text-white hover:bg-blue-500 transition-colors ml-auto"
+              className="bg-gray-800 py-1 px-3 rounded-lg text-white hover:bg-gray-900 transition-colors ml-auto"
               style={{ minWidth: '100px' }}
               onClick={() => window.location.href = adminUrl}
             >
@@ -197,12 +199,12 @@ export default function Home() {
           <div>
             <div className="flex items-center justify-between mb-2">
               <div className="flex items-center">
-                <h3 className="text-2xl font-semibold leading-6 text-gray-100">
+                <h3 className="text-2xl font-semibold leading-6 text-gray-800">
                   Credential: {displayResult()}
                 </h3>
                 <ArrowPathIcon
                   onClick={() => reloadMLInference()}
-                  className="mx-auto h-6 w-6 ml-4 cursor-pointer text-gray-400 hover:text-white transition-colors"
+                  className="mx-auto h-6 w-6 ml-4 cursor-pointer text-gray-600 hover:text-gray-800 transition-colors"
                 />
                 {isMLProofVerified && (
                   <CheckBadgeIcon
@@ -225,36 +227,36 @@ export default function Home() {
             </svg>
 
             <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-              <div className="relative overflow-hidden rounded-lg bg-gray-800 px-4 pb-12 pt-5 shadow border border-gray-700 sm:px-6 sm:pt-6">
+              <div className="relative overflow-hidden rounded-lg bg-white px-4 pb-12 pt-5 shadow border border-gray-300 sm:px-6 sm:pt-6">
                 <dt>
-                  <div className="absolute rounded-md bg-indigo-500 p-3">
+                  <div className="absolute rounded-md bg-gray-800 p-3">
                     <CheckBadgeIcon className="h-6 w-6 text-white" />
                   </div>
-                  <p className="ml-16 truncate text-sm font-medium text-gray-400">
+                  <p className="ml-16 truncate text-sm font-medium text-gray-800">
                     Discord Score
                   </p>
                 </dt>
                 <dd className="mt-1 ml-16 flex flex-col items-start pb-6 sm:pb-7">
-                  <p className="text-2xl font-semibold text-gray-100">{discordScore}</p>
+                  <p className="text-2xl font-semibold text-gray-800">{discordScore}</p>
                   <div className="mt-4 flex space-x-4">
                     <button
-                      className="bg-indigo-600 text-white py-1 px-2 rounded-md text-sm hover:bg-indigo-700 transition-colors"
+                      className="bg-gray-800 text-white py-1 px-2 rounded-md text-sm hover:bg-gray-900 transition-colors"
                       onClick={() => zkPassStart()}
                     >
                       {discordOwnerVerified ? "Discord Account Owner Verified!" : "Verify Discord Account Owner"}
                     </button>
                     <button
-                      className="bg-indigo-600 text-white py-1 px-2 rounded-md text-sm hover:bg-indigo-700 transition-colors"
+                      className="bg-gray-800 text-white py-1 px-2 rounded-md text-sm hover:bg-gray-900 transition-colors"
                       onClick={() => console.log("verify discord activity")}
                     >
                       Verify Discord Activity
                     </button>
                   </div>
-                  <div className="absolute inset-x-0 bottom-0 bg-gray-700 px-4 py-4 sm:px-6">
+                  <div className="absolute inset-x-0 bottom-0 bg-gray-100 px-4 py-4 sm:px-6">
                     <div className="text-sm">
                       <a
                         href="#"
-                        className="font-medium text-indigo-400 hover:text-indigo-300"
+                        className="font-medium text-blue-600 hover:text-blue-500"
                       >
                         View source
                         <span className="sr-only"> stats</span>
@@ -264,17 +266,17 @@ export default function Home() {
                 </dd>
               </div>
 
-              <div className="relative overflow-hidden rounded-lg bg-gray-800 px-4 pb-12 pt-5 shadow border border-gray-700 sm:px-6 sm:pt-6">
+              <div className="relative overflow-hidden rounded-lg bg-white px-4 pb-12 pt-5 shadow border border-gray-300 sm:px-6 sm:pt-6">
                 <dt>
-                  <div className="absolute rounded-md bg-indigo-500 p-3">
+                  <div className="absolute rounded-md bg-gray-800 p-3">
                     <CheckBadgeIcon className="h-6 w-6 text-white" />
                   </div>
-                  <p className="ml-16 truncate text-sm font-medium text-gray-400">
+                  <p className="ml-16 truncate text-sm font-medium text-gray-800">
                     WorldCoin
                   </p>
                 </dt>
                 <dd className="mt-1 ml-16 flex flex-col items-start pb-6 sm:pb-7">
-                  <p className="text-2xl font-semibold text-gray-100">{worldcoinScore}</p>
+                  <p className="text-2xl font-semibold text-gray-800">{worldcoinScore}</p>
                   <div className="mt-4 flex space-x-4">
                   <IDKitWidget
                     action={action!}
@@ -284,17 +286,17 @@ export default function Home() {
                     verification_level={VerificationLevel.Orb} // Change this to VerificationLevel.Device to accept Orb- and Device-verified users
                   />
                     <button
-                      className="bg-indigo-600 text-white py-1 px-2 rounded-md text-sm hover:bg-indigo-700 transition-colors"
+                      className="bg-gray-800 text-white py-1 px-2 rounded-md text-sm hover:bg-gray-900 transition-colors"
                       onClick={() => setOpen(true)}
                     >
                       {worldcoinVerified ? "Verified!" : "Verify with World ID"}
                     </button>
                   </div>
-                  <div className="absolute inset-x-0 bottom-0 bg-gray-700 px-4 py-4 sm:px-6">
+                  <div className="absolute inset-x-0 bottom-0 bg-gray-100 px-4 py-4 sm:px-6">
                     <div className="text-sm">
                       <a
                         href="#"
-                        className="font-medium text-indigo-400 hover:text-indigo-300"
+                        className="font-medium text-blue-600 hover:text-blue-500"
                       >
                         View source
                         <span className="sr-only"> stats</span>
@@ -307,32 +309,32 @@ export default function Home() {
               {stats.map((item) => (
                 <div
                   key={item.name}
-                  className="relative overflow-hidden rounded-lg bg-gray-800 px-4 pb-12 pt-5 shadow border border-gray-700 sm:px-6 sm:pt-6"
+                  className="relative overflow-hidden rounded-lg bg-white px-4 pb-12 pt-5 shadow border border-gray-300 sm:px-6 sm:pt-6"
                 >
                   <dt>
-                    <div className="absolute rounded-md bg-indigo-500 p-3">
+                    <div className="absolute rounded-md bg-gray-800 p-3">
                       <item.icon
                         className="h-6 w-6 text-white"
                         aria-hidden="true"
                       />
                     </div>
-                    <p className="ml-16 truncate text-sm font-medium text-gray-400">
+                    <p className="ml-16 truncate text-sm font-medium text-gray-800">
                       {item.name}
                     </p>
                   </dt>
                   <dd className="mt-1 ml-16 flex items-center pb-6 sm:pb-7">
-                    <p className="text-2xl font-semibold text-gray-100">
+                    <p className="text-2xl font-semibold text-gray-800">
                       {item.stat}
                     </p>
                     <CheckBadgeIcon
                       className="h-8 w-8 ml-2"
                       style={{ fill: "url(#grad)" }}
                     />
-                    <div className="absolute inset-x-0 bottom-0 bg-gray-700 px-4 py-4 sm:px-6">
+                    <div className="absolute inset-x-0 bottom-0 bg-gray-100 px-4 py-4 sm:px-6">
                       <div className="text-sm">
                         <a
                           href="#"
-                          className="font-medium text-indigo-400 hover:text-indigo-300"
+                          className="font-medium text-blue-600 hover:text-blue-500"
                         >
                           View source
                           <span className="sr-only"> {item.name} stats</span>
@@ -347,7 +349,7 @@ export default function Home() {
               <div className="w-full">
                 <p className="font-semibold">Step1: Generate ZKML proof</p>
                 <button
-                  className="w-full bg-indigo-600 text-white py-2 mt-2 rounded-lg flex items-center justify-center hover:bg-indigo-700 transition-colors"
+                  className="w-full bg-gray-800 text-white py-2 mt-2 rounded-lg flex items-center justify-center hover:bg-gray-900 transition-colors"
                   onClick={generateMLProof}
                   disabled={isProofGenerating}
                 >
@@ -357,15 +359,15 @@ export default function Home() {
                   {isProofGenerating ? "Generating..." : "Generate"}
                 </button>
                 {mlProof && (
-                  <div className="mt-4 bg-gray-800 border border-gray-700 p-4 rounded-lg max-h-64 max-w-lg overflow-y-auto">
-                    <pre className="text-sm text-gray-300 whitespace-pre-wrap">{mlProof}</pre>
+                  <div className="mt-4 bg-white border border-gray-300 p-4 rounded-lg max-h-64 max-w-lg overflow-y-auto">
+                    <pre className="text-sm text-gray-800 whitespace-pre-wrap">{mlProof}</pre>
                   </div>
                 )}
               </div>
               <div>
                 <p className="font-semibold">Step2: Verify ZKML Proof</p>
                 <button
-                  className="w-full bg-indigo-600 py-2 mt-2 rounded-lg text-white hover:bg-indigo-700 transition-colors"
+                  className="w-full bg-gray-800 py-2 mt-2 rounded-lg text-white hover:bg-gray-900 transition-colors"
                   onClick={verifyMLProof}
                 >
                   {isMLProofVerified ? "Verified Successfully" : "Verify"}
@@ -376,7 +378,7 @@ export default function Home() {
                 {isMLProofVerified && (
                   <div className="mt-4">
                     <button
-                      className="w-full bg-green-600 py-2 rounded-lg text-white hover:bg-green-700 transition-colors"
+                      className="w-full bg-gray-800 py-2 rounded-lg text-white hover:bg-gray-900 transition-colors"
                       onClick={() => window.location.href = votingUrl}
                     >
                       Go to Voting Page
